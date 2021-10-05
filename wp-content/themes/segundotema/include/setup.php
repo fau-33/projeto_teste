a7303dc9cec512dfdf80c4a1c031fe9ca48e6e9f
Flávio Félix <fau_felix30@hotmail.com>
2021-10-04 21:56:31 -0300

    Feito o menu e o navegation da página

--- wp-content/themes/segundotema/include/setup.php
+++ wp-content/themes/segundotema/include/setup.php
@@ -23,5 +23,9 @@ function bs_after_setup() {
 
 	add_theme_support('title-tag');
 
+	add_theme_support('custom-logo');
+
+	add_theme_support('post-thumbnails');
+
 	register_nav_menu("primary", __('Primary Menu', 'segundatema'));
 }
